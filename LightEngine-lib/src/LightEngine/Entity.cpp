e9f5b19d8d73c35416484b76fb68f8c282ed6888
Raphael FELIX <rfelix@gaming.tech>
2026-03-11 15:06:05 +0100

    ?

--- LightEngine-lib/src/LightEngine/Entity.cpp
+++ LightEngine-lib/src/LightEngine/Entity.cpp
@@ -201,7 +201,6 @@ bool Entity::GoToPosition(int x, int y, float speed)
 {
 	sf::Vector2i worldPos = sf::Vector2i(GameManager::Get()->mpWindow->mapPixelToCoords(sf::Vector2i(x, y)));
 
-
 	if (GoToDirection(worldPos.x, worldPos.y, speed) == false)
 		return false;
 
