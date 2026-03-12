2613095c59384c3c1740e21a354c9e1fb28d5ff7
Paul-THIEBAUX <pthiebaux@gaming.tech>
2026-03-12 11:23:38 +0100

    state machine

--- LightEngine-lib/ide/vs/Mini_studio/StateMachine.cpp
+++ LightEngine-lib/ide/vs/Mini_studio/StateMachine.cpp
@@ -1,1 +1,35 @@
 #include "StateMachine.h"
+
+void StateMachine::change(int nbr) {
+	if (nbr == 0) {
+		state(Idle);
+	}
+	else if (nbr == 1) {
+		state(Moving);
+	}
+	else if (nbr == 2) {
+		state(Reload);
+	}
+	else if (nbr == 3) {
+		state(Stun);
+	}
+	else if (nbr == 4) {
+		state(AttackFall);
+	}
+	else if (nbr == 5) {
+		state(AttackBull);
+	}
+	else if (nbr == 6) {
+		state(AttackSmart);
+	}
+	else if (nbr == 7) {
+		state(AttackPunch);
+	}
+	else if (nbr == 8) {
+		state(AttackPatern);
+	}
+}
+
+void StateMachine::effect() {
+
+}
--- LightEngine-lib/ide/vs/Mini_studio/StateMachine.h
+++ LightEngine-lib/ide/vs/Mini_studio/StateMachine.h
@@ -1,18 +1,28 @@
 #pragma once
 class StateMachine
 {
+private:
+
+protected:
 	enum state {
-		Idle,
-		Moving,
-		Reload,
-		Stun,
-		AttackFall,
-		AttackBull,
-		AttackSmart,
-		AttackPunch,
-		AttackPatern,
+		Idle,            /*0 = ne bouge pas, ne fait rien.*/
+		Moving,          /*1 = bouge.*/
+		Reload,          /*2 = recharge.*/
+		Stun,            /*3 = est incapable de bouger ou de faire quoi que se soit.*/
+		AttackFall,      /*4 = attaque de la zone metal ennemi 1.*/
+		AttackBull,      /*5 = attaque de la zone metal ennemi 2.*/
+		AttackSmart,     /*6 = attaque de la zone jazz ennemi 1.*/
+		AttackPunch,     /*7 = attaque de la zone jazz ennemi 2.*/
+		AttackPatern,    /*8 = merci de spesifiez le paterne grace a la rose des vents (N,S,E,W).*/
 		
 		count
 	};
+
+public:
+
+	void change(int nbr);
+
+	void effect();
+
 };
 
