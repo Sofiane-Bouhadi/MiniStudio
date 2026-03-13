ce2b2cbaeba6de00f55d196336487e56fd1b845d
Raphael FELIX <rfelix@gaming.tech>
2026-03-13 10:21:04 +0100

    SOund

--- LightEngine-lib/src/LightEngine/Sound.cpp
+++ LightEngine-lib/src/LightEngine/Sound.cpp
@@ -1,1 +1,26 @@
 #include "Sound.h"
+
+Sound::Sound(const char* path)
+{
+
+}
+
+void Sound::Play()
+{
+	mSound->play();
+}
+
+void Sound::Pause()
+{
+	mSound->pause();
+}
+
+void Sound::Stop()
+{
+	mSound->stop();
+}
+
+void Sound::SetLoop(bool isLoop)
+{
+	mSound->setLoop(isLoop);
+}
--- LightEngine-lib/src/LightEngine/Sound.h
+++ LightEngine-lib/src/LightEngine/Sound.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <SFML/Audio/Sound.hpp>
+
 class Sound
 {
+private:
+	sf::Sound* mSound;
+
+public:
+	Sound(const char* path);
+	
+	void Play();
+	void Pause();
+	void Stop();
+	void SetLoop(bool isLoop);
 };
 
