219f7a4943bcd8c3a6ef472d9d8c058cc9669c90
Raphael FELIX <rfelix@gaming.tech>
2026-03-13 09:51:04 +0100

    jumpStrength

--- LightEngine-lib/src/LightEngine/GravityEntity.h
+++ LightEngine-lib/src/LightEngine/GravityEntity.h
@@ -5,12 +5,14 @@ class GravityEntity : public Entity
 {
 protected:
 	float mGravityStrength = 9.81f;
-	float mYVelocity = 0;
+	float mYVelocity = 0.f;
+	float mJumpStrength = 100.f;
 
 public:
 	void Update() override;
 	void Jump();
 
 	void SetGravityStrength(float gravityStrength) { mGravityStrength = gravityStrength; }
+	void SetJumpStrength(float jumpStrength) { mJumpStrength = jumpStrength; }
 };
 
