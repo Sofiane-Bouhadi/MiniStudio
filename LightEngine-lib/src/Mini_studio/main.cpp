de59d3f7d51ea90cf88c77352ccba962ffd3dbdf
Paul-THIEBAUX <pthiebaux@gaming.tech>
2026-03-17 16:16:51 +0100

    MainScene -> SceneTemporaire

--- LightEngine-lib/src/Mini_studio/main.cpp
+++ LightEngine-lib/src/Mini_studio/main.cpp
@@ -6,6 +6,7 @@
 
 #include "GameManager.h"
 #include "MainScene.h"
+#include "sceneTemporaire.h"
 
 #include <cstdlib>
 #include <crtdbg.h>
@@ -19,9 +20,9 @@ int main()
 
 	GameManager* pInstance = GameManager::Get();
 
-	pInstance->CreateWindow(1280, 720, "MainScene", 60, sf::Color::Black);
+	pInstance->CreateWindow(1280, 720, "Scene", 60, sf::Color::Black);
 
-	pInstance->LaunchScene<MainScene>();
+	pInstance->LaunchScene<sceneTemporaire>();
 
 	return 0;
 }
