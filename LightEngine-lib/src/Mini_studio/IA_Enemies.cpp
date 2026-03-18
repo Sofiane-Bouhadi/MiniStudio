9e82da3cc2cc92b4a1812e10fabe43e053373bef
Paul-THIEBAUX <pthiebaux@gaming.tech>
2026-03-18 20:36:11 +0100

    update

--- LightEngine-lib/src/Mini_studio/IA_Enemies.cpp
+++ LightEngine-lib/src/Mini_studio/IA_Enemies.cpp
@@ -1,58 +1,41 @@
 #include "IA_Enemies.h"
 
-void IA::liveBull(enemies* pEnemy) {
-
-	StateMachine state;
-	sf::Vector2f positionEnemy = pEnemy-> GetPosition();
-	float distence = pEnemy->telemetrie();
-	if (distence <= 500 && state.canAttack && state.canMove) {
-		pEnemy->AttackBull();
-		state.change(5);
-	}
-	else {
-		pEnemy->moveingInLigne(0,100);
-		state.change(1);
-	}
-}
+//void IA::liveBull(enemies* pEnemy) {
+//
+//	StateMachine state;
+//	sf::Vector2f positionEnemy = pEnemy-> GetPosition();
+//	float distence = pEnemy->telemetrie();
+//	if (distence <= 500 && state.canAttack && state.canMove) {
+//		pEnemy->AttackBull();
+//		state.change(5);
+//	}
+//	else {
+//		pEnemy->moveingInLigne(0,100);
+//		state.change(1);
+//	}
+//}
 
 void IA::liveFall(enemies* pEnemy) {
-
-	StateMachine state;
-	sf::Vector2f positionEnemy = pEnemy->GetPosition();
-	float distence = pEnemy->telemetrie();
-	if (distence <= 500 && state.canAttack) {
 		pEnemy->AttackFall();
-		state.change(4);
-	}
-	else {
-		state.change(0);
-	}
+		std::cout << "fall attaque" << std::endl;
 }
 
-void IA::livePunch(enemies* pEnemy) {
-
-	StateMachine state;
-	sf::Vector2f positionEnemy = pEnemy->GetPosition();
-	float distence = pEnemy->telemetrie();
-	if (distence <= 500 && state.canAttack) {
-		pEnemy->AttackPunch();
-		state.change(7);
-	}
-	else {
-		state.change(0);
-	}
-}
+//void IA::livePunch(enemies* pEnemy) {
+//
+//	StateMachine state;
+//	sf::Vector2f positionEnemy = pEnemy->GetPosition();
+//	float distence = pEnemy->telemetrie();
+//	if (distence <= 500 && state.canAttack) {
+//		pEnemy->AttackPunch();
+//
+//		state.change(7);
+//	}
+//	else {
+//		state.change(0);
+//	}
+//}
 
 void IA::liveShot(enemies* pEnemy) {
-
-	StateMachine state;
-	sf::Vector2f positionEnemy = pEnemy->GetPosition();
-	float distence = pEnemy->telemetrie();
-	if (distence <= 500 && state.canAttack) {
-		pEnemy->AttackSmart();
-		state.change(4);
-	}
-	else {
-		state.change(0);
-	}
+	pEnemy->AttackSmart();
+	std::cout << "smart attaque" << std::endl;
 }
